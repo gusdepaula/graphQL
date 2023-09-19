35734d043d77f8dd4cfaeaaea33420b0c6d244d6
Gustavo de Paula <gdepaula@gmail.com>
2023-09-19 11:16:26 -0300

    Retornando Array #02

--- projeto/index.js
+++ projeto/index.js
@@ -1,5 +1,26 @@
 const { ApolloServer, gql } = require("apollo-server");
 
+const usuarios = [
+  {
+    id: 1,
+    nome: "João Silva",
+    email: "jsilva@email.com",
+    idade: 29,
+  },
+  {
+    id: 2,
+    nome: "Maria Silva",
+    email: "msilva@email.com",
+    idade: 25,
+  },
+  {
+    id: 3,
+    nome: "José da Silva",
+    email: "zesilva@email.com",
+    idade: 55,
+  },
+];
+
 const typeDefs = gql`
   scalar Date
 
@@ -26,6 +47,7 @@ const typeDefs = gql`
     usuarioLogado: Usuario
     produtoEmDestaque: Produto
     numerosMegaSena: [Int!]!
+    usuarios: [Usuario]
   }
 `;
 
@@ -75,6 +97,9 @@ const resolvers = {
         .fill(0)
         .map(() => parseInt(Math.random() * 60 + 1))
         .sort(crescente);
+    },
+    usuarios() {
+      return usuarios;
     },
   },
 };
