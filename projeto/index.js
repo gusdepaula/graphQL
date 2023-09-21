5d4799aa0a208ea554f052a7f5e5c6fe1e43307c
Gustavo de Paula <gdepaula@gmail.com>
2023-09-21 10:59:44 -0300

    Usando Resolver para relacionar Tipos

--- projeto/index.js
+++ projeto/index.js
@@ -3,6 +3,7 @@ const { ApolloServer, gql } = require("apollo-server");
 const perfis = [
   { id: 1, nome: "comum" },
   { id: 2, nome: "administrador" },
+  { id: 3, nome: "administrador" },
 ];
 
 const usuarios = [
@@ -11,18 +12,21 @@ const usuarios = [
     nome: "João Silva",
     email: "jsilva@email.com",
     idade: 29,
+    perfil_id: 1,
   },
   {
     id: 2,
     nome: "Maria Silva",
     email: "msilva@email.com",
     idade: 25,
+    perfil_id: 2,
   },
   {
     id: 3,
     nome: "José da Silva",
     email: "zesilva@email.com",
     idade: 55,
+    perfil_id: 3,
   },
 ];
 
@@ -41,6 +45,7 @@ const typeDefs = gql`
     idade: Int
     salario: Float
     vip: Boolean
+    perfil: Perfil
   }
 
   type Produto {
@@ -68,6 +73,10 @@ const resolvers = {
   Usuario: {
     salario(usuario) {
       return usuario.salario_real;
+    },
+    perfil(usuario) {
+      const selecionados = perfis.filter((p) => p.id === usuario.perfil_id);
+      return selecionados ? selecionados[0] : null;
     },
   },
   Produto: {
