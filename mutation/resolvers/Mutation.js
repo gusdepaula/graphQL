1979abd8bffe8524755568d35c21720f07a36485
Gustavo de Paula <gdepaula@gmail.com>
2023-09-25 11:10:08 -0300

    Validar E-mail para Evitar Usuários Duplicados

--- mutation/resolvers/Mutation.js
+++ mutation/resolvers/Mutation.js
@@ -1,12 +1,16 @@
 const { usuarios, proximoId } = require("../data/db");
 
 module.exports = {
-  novoUsuario(_, { nome, email, idade }) {
+  novoUsuario(
+    _,
+    //{ nome, email, idade }
+    args
+  ) {
+    const emailExistente = usuarios.some((u) => u.email === args.email);
+    if (emailExistente) throw new Error("E-mail já cadastrado");
     const novo = {
       id: proximoId(),
-      nome,
-      email,
-      idade,
+      ...args,
       perfil_id: 1,
       status: "ATIVO",
     };
