d5a05eb21769ce8dc694b3b89ccf180a29d14605
Gustavo de Paula <gdepaula@gmail.com>
2023-09-27 14:55:04 -0300

    Migrations #03

--- banco-de-dados/migrations/20230927132742_tabela_usuarios_perfis.js
+++ banco-de-dados/migrations/20230927132742_tabela_usuarios_perfis.js
@@ -1,15 +1,3 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
-};
+exports.up = function (knex, Promise) {};
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex, Promise) {};
