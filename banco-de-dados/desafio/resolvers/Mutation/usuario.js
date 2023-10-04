8a5dc65145593b52d9f6d26e40bf8545d6ccc2eb
Gustavo de Paula <gdepaula@gmail.com>
2023-10-04 10:06:59 -0300

    Desafio Banco de Dados & GraphQL - Reposta #03

--- banco-de-dados/desafio/resolvers/Mutation/usuario.js
+++ banco-de-dados/desafio/resolvers/Mutation/usuario.js
@@ -1,13 +1,41 @@
-const db = require('../../config/db')
+const db = require("../../config/db");
+const { perfil: obterPerfil } = require("../Query/perfil");
 
 module.exports = {
-    async novoUsuario(_, { dados }) {
-        // Implementar
-    },
-    async excluirUsuario(_, { filtro }) {
-        // Implementar
-    },
-    async alterarUsuario(_, { filtro, dados }) {
-        // Implementar
+  async novoUsuario(_, { dados }) {
+    try {
+      const idsPerfis = [];
+      if (dados.perfis) {
+        for (filtro of dados.perfis) {
+          const perfil = await obterPerfil(_, {
+            filtro,
+          });
+          if (perfil) idsPerfis.push(perfil.id);
+        }
+      }
+      delete dados.perfis;
+
+      const [id] = await db("usuarios").insert(dados);
+
+      for (perfil_id of idsPerfis) {
+        await db("usuarios_perfis").insert({ perfil_id, usuario_id: id });
+
+        return db("usuarios").where({ id }).first();
+      }
+    } catch (e) {
+      throw new Error(e.sqlMessage);
     }
-}
+  },
+  async excluirUsuario(_, { filtro }) {
+    try {
+    } catch (e) {
+      throw new Error(e.sqlMessage);
+    }
+  },
+  async alterarUsuario(_, { filtro, dados }) {
+    try {
+    } catch (e) {
+      throw new Error(e.sqlMessage);
+    }
+  },
+};
