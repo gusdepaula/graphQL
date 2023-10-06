3424406cc4ddcb05536b890adb0d2ca61ab356d3
Gustavo de Paula <gdepaula@gmail.com>
2023-10-06 11:54:02 -0300

    Login de Usuário

--- projeto-final/backend/resolvers/Query/usuario.js
+++ projeto-final/backend/resolvers/Query/usuario.js
@@ -1,22 +1,74 @@
-const db = require('../../config/db')
+const db = require("../../config/db");
+const bcrypt = require("bcrypt-nodejs");
+const { getUsuarioLogado } = require("../comum/usuario");
 
 module.exports = {
-    usuarios() {
-        return db('usuarios')
-    },
-    usuario(_, { filtro }) {
-        if(!filtro) return null
-        const { id, email } = filtro
-        if(id) {
-            return db('usuarios')
-                .where({ id })
-                .first()
-        } else if(email) {
-            return db('usuarios')
-                .where({ email })
-                .first()
-        } else {
-            return null
-        }
-    },
-}
+  async login(_, { dados }) {
+    const usuario = await db("usuarios").where({ email: dados.email }).first();
+
+    if (!usuario) {
+      throw new Error("Usuário/Senha inválido");
+    }
+
+    const saoIguais = bcrypt.compareSync(dados.senha, usuario.senha);
+
+    if (!saoIguais) {
+      throw new Error("Usuário/Senha inválido");
+    }
+
+    return getUsuarioLogado(usuario);
+  },
+  usuarios() {
+    return db("usuarios");
+  },
+  usuario(_, { filtro }) {
+    if (!filtro) return null;
+    const { id, email } = filtro;
+    if (id) {
+      return db("usuarios").where({ id }).first();
+    } else if (email) {
+      return db("usuarios").where({ email }).first();
+    } else {
+      return null;
+    }
+  },
+};
+
+// const db = require("../../config/db");
+// const bcrypt = require("bcrypt-nodejs");
+// const { getUsuarioLogado } = require("../comum/usuario");
+
+// module.exports = {
+//   async login(_, { dados }) {
+//     const usuario = await db("usuarios").where({ email: dados.email }).first();
+
+//     if (!usuario) {
+//       throw new Error("Usuário/Senha inválido");
+//     }
+
+//     const saoIguais = bcrypt.compareSync(dados.senha, usuario.senha);
+
+//     if (!saoIguais) {
+//       throw new Error("Usuário/Senha inválido");
+//     }
+
+//     return getUsuarioLogado(usuario);
+//   },
+//   usuarios(parent, args, ctx) {
+//     ctx && ctx.validarAdmin();
+//     return db("usuarios");
+//   },
+//   usuario(_, { filtro }, ctx) {
+//     ctx && ctx.validarUsuarioFiltro(filtro);
+
+//     if (!filtro) return null;
+//     const { id, email } = filtro;
+//     if (id) {
+//       return db("usuarios").where({ id }).first();
+//     } else if (email) {
+//       return db("usuarios").where({ email }).first();
+//     } else {
+//       return null;
+//     }
+//   },
+// };
