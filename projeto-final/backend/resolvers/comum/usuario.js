d1b5960ff91902ad49dd4459fa066252c17fc28d
Gustavo de Paula <gdepaula@gmail.com>
2023-10-09 10:56:29 -0300

    Usando Context #02

--- projeto-final/backend/resolvers/comum/usuario.js
+++ projeto-final/backend/resolvers/comum/usuario.js
@@ -15,10 +15,9 @@ module.exports = {
       exp: agora + 3 * 24 * 60 * 60,
     };
 
-    const authSecret = process.env.APP_AUTH_SECRET;
     return {
       ...usuarioInfo,
-      token: jwt.encode(usuarioInfo, authSecret),
+      token: jwt.encode(usuarioInfo, process.env.APP_AUTH_SECRET),
     };
   },
 };
