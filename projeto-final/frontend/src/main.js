9fbbfc816ac18028127c31489990a8be6bd1bb47
Gustavo de Paula <gdepaula@gmail.com>
2023-10-10 15:45:44 -0300

    Configuração Apollo Client

--- projeto-final/frontend/src/main.js
+++ projeto-final/frontend/src/main.js
@@ -1,11 +1,12 @@
-import Vue from 'vue'
-import './plugins/vuetify'
-import App from './App.vue'
-import store from './store'
+import Vue from "vue";
+import "./plugins/vuetify";
+import "./plugins/graphql";
+import App from "./App.vue";
+import store from "./store";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
