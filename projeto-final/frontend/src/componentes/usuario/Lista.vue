f95885591affcc44a177d214c9fd7efac616aff1
Gustavo de Paula <gdepaula@gmail.com>
2023-10-16 09:36:39 -0300

    Login de usuário

--- projeto-final/frontend/src/componentes/usuario/Lista.vue
+++ projeto-final/frontend/src/componentes/usuario/Lista.vue
@@ -32,6 +32,7 @@
 
 <script>
 import Erros from "../comum/Erros";
+import gql from "graphql-tag";
 
 export default {
   components: { Erros },
@@ -49,8 +50,31 @@ export default {
   },
   methods: {
     obterUsuarios() {
-      //
-      console.log(this.$api);
+      this.$api
+        .query({
+          query: gql`
+            query {
+              usuarios {
+                id
+                nome
+                email
+                perfis {
+                  nome
+                  rotulo
+                }
+              }
+            }
+          `,
+          fetchPolicy: "network-only",
+        })
+        .then((resultado) => {
+          this.usuarios = resultado.data.usuarios;
+          this.erros = null;
+        })
+        .catch((e) => {
+          this.usuarios = [];
+          this.erros = e;
+        });
     },
   },
 };
