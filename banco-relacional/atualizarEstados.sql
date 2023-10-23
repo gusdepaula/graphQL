ea5e32662d8431ae5a7ff01005f27a4169e92176
Gustavo de Paula <gdepaula@gmail.com>
2023-10-23 14:02:06 -0300

    Atualizar Estados

---
+++ banco-relacional/atualizarEstados.sql
@@ -0,0 +1,5 @@
+-- UPDATE estados set nome = 'Maranhão' WHERE sigla 'MA'
+
+-- SELECT est.`nome` FROM estados est WHERE sigla 'MA'
+
+UPDATE estados SET nome = 'Paraná', populacao = 11.32 WHERE sigla 'PR'
