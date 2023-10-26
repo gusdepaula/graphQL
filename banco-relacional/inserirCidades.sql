62ec6f662cf64f3482be85912c730c71825ace9f
Gustavo de Paula <gdepaula@gmail.com>
2023-10-26 11:15:21 -0300

    Inserir Cidades

---
+++ banco-relacional/inserirCidades.sql
@@ -0,0 +1,7 @@
+-- INSERT INTO cidades (nome, area, estado_id) VALUES ('Campinas', 795, 27 )
+
+-- INSERT INTO cidades (nome, area, estado_id) VALUES ('Niterói', 133.9, 21 )
+
+-- INSERT INTO cidades (nome, area, estado_id) VALUES ('Caruaru', 920.6, 19 )
+
+INSERT INTO cidades (nome, area, estado_id) VALUES ('Juazeiro do Norte', 248.2, 8 )
