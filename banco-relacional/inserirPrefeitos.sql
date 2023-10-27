fec1e3534d8c84f8152a23d6b37c4ecaeb0309c0
Gustavo de Paula <gdepaula@gmail.com>
2023-10-27 10:29:25 -0300

    Inserir Prefeitos

---
+++ banco-relacional/inserirPrefeitos.sql
@@ -0,0 +1,9 @@
+-- SELECT * from cidades;
+
+-- INSERT INTO prefeitos (nome, cidade_id) VALUES ('Rodrigo Neves', 3), ('Raquel Lyra', 4);
+
+-- INSERT INTO prefeitos (nome, cidade_id) VALUES ('Zenaldo Coutinho', null)
+
+-- INSERT INTO prefeitos (nome, cidade_id) VALUES ('Rafael Greca', null)
+
+INSERT INTO prefeitos (nome, cidade_id) VALUES ('Rodrigo Pinheiro', 4)
